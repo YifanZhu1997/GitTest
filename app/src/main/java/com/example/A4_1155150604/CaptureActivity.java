bb6f0475251a9214964e9b39b3d8e9d04bc4c27b
YifanZhu1997 <1155150604@link.cuhk.edu.hk>
2021-04-16 14:39:37 +0800

    test2

--- app/src/main/java/com/example/A4_1155150604/CaptureActivity.java
+++ app/src/main/java/com/example/A4_1155150604/CaptureActivity.java
@@ -8,5 +8,7 @@ public class CaptureActivity extends com.journeyapps.barcodescanner.CaptureActiv
 /*fuck you again*/
     /*fuck you again*/
     /*fuck you again*/
+    /*fuck you again*/
+    /*fuck you again*/ /*test2*/
 }
 
